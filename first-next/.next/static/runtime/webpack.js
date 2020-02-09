e472a1dd77b56c6732241dd5b4052f099bef4ceb
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-09 12:52:00 +0100

    import and render Navabr inside contact page

--- first-next/.next/static/runtime/webpack.js
+++ first-next/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "87edab32b94fe1a089ff";
+/******/ 	var hotCurrentHash = "1c268f767e9837eaacf9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- first-next/pages/contact.js
+++ first-next/pages/contact.js
@@ -1,8 +1,13 @@
-const ContactPage = () => (
-  <div>
-    <h1>ContactPage!!!!</h1>
-    <p>This is contact page - do not contact us</p>
-  </div>
-)
+import Navbar from '../components/Navbar'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>ContactPage!!!!</h1>
+      <p>This is contact page - do not contact us</p>
+    </div>
+  )
+}
 
 export default ContactPage
